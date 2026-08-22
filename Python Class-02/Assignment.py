5f66ab08f37544475903b16b0e5c63224c01a4af
AdarshAnand-cg <adarsh.anand.cg@gmail.com>
2026-08-22 17:05:35 +0530

    Assignment Done

---
+++ Python Class-02/Assignment.py
@@ -0,0 +1,44 @@
+#Question-24
+my_name="Adarsh Anand"
+my_age=17
+city_name="Muzaffarpur"
+print(my_name,my_age,city_name)
+
+#Question-25
+Student_name="Adarsh Anand"
+Student_Roll_Number=39
+student_branch="Computer Science and Engineering"
+print(Student_name,Student_Roll_Number,student_branch)
+
+#Question-26
+marks=470
+marks=428
+print(marks)
+print("Marks before reassignment is 470 and after reassignment is 428.")
+
+#Question-27
+name="Rahul"
+age=18
+city="Patna"
+print(name,age,city)
+
+#Question-28
+x=0
+y=0
+z=0
+print(x,y,z)
+
+#Question-29
+student="Rahul"
+student_name="Rahul"
+branch= "B.Tech"
+print(student,student_name,branch)
+
+#Question-30
+#Student Info.
+student_name1="Raghav"
+student_age1=19
+student_age1=20
+print(student_name1,student_age1)
+
+

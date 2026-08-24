6ed28e20f8be0f27b2482df4f67f706bfc334277
AdarshAnand-cg <adarsh.anand.cg@gmail.com>
2026-08-24 18:18:07 +0530

    Assignment of 3rd Class is Completed

---
+++ Python Class-03/Assignment.py
@@ -0,0 +1,50 @@
+# Question-24
+Name="Adarsh Anand"
+Age=18
+Height=6.7
+is_student=True
+Result=None
+print(type(Name),type(Age),type(Height),type(is_student),type(Result))
+
+# Question-25
+a=50
+b=50.0
+c="50"
+print(type(a),type(b),type(c))
+
+# Question-26
+a=True
+b="True"
+print(type(a),type(b))
+
+# Question-27
+a=None
+b="None"
+print(type(a))
+print(type(b))
+
+# Question-28
+value=20
+print(type(value))
+value="20"
+print(type(value))
+
+# Question-29
+product_name="string"
+product_quantity=5
+product_price=599.99
+product_available=True
+product_discount=None
+print(type(product_name),type(product_quantity),type(product_price),type(product_available),type(product_discount))
+
+# Question-30
+a=10
+b=10.0
+c="10"
+A=True
+B="True"
+C=None
+d="None"
+print(type(a),type(b),type(c),type(A),type(B),type(C),type(d))
+
+
---
+++ Python Class-03/demo.py
@@ -0,0 +1,33 @@
+# Trying to check about constant
+NAME="Adarsh"
+NAME="Anand"
+NAME="Hello"
+print(NAME)
+print(type(NAME))
+
+# Another Demo
+Name="Adarsh"
+Age=17
+AGe="17"
+AgE=17.0
+Info="Here 'Age' has integer data type , 'AGe' has string data type and 'AgE' has Float Data type"
+print(type(Age),type(AGe),type(AgE))
+
+# A New Concept
+age=22
+aGe="22"
+int_to_string=str(age)
+print(age)
+print(type(int_to_string))
+Information="Here age is actually a integer Data type ,but we change it data type from integer to string"
+name="Tony"
+name=int(name)
+print("Converting str into int",type(name))
+Information="Here,We cant make String into integer"
+
+# Converting Float into Integer
+number=7.9
+number=int(7.9)
+print("converting float into int",type(number))
+
+

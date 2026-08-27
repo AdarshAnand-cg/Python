9357006708b97f253f911d50a2b48a27a9172d2e
AdarshAnand-cg <adarsh.anand.cg@gmail.com>
2026-08-27 23:40:45 +0530

    Practice work of class 5

---
+++ Python Class-05/Demo.py
@@ -0,0 +1,34 @@
+# INDEXING
+a="Cristiano Ronaldo"
+b="Cristiano Ronaldo"
+print(a==b)
+Info="the double equal to called as 'compare to' ,it compare between the values (which is mentioined) whether they are identical or not by saying True or False"
+
+c="Hello World"
+print(c[6])
+info="The big bracket shows indexing, basically numbering of characters in string from 0 to last character(even space) from left to right,And, starts from -1 from right to left to first character."
+Example="In line 6==>H=0,e=1,l=2,l=3,o=4, (space)=5,W=6 and like that." \
+"so,the 6th index is W,the run shows 'W'.."
+
+print(len(c))
+info="It shows lenght of String"
+
+print(c[0:8:4])
+Info="The index before colon is starting point(if is blanked it starts with the initial of string) and the index after colon is one next to the ending point,basically it count the lenght no. text(if blanked then till last character of the string)"
+
+# String Concatenation
+x="Sherlock"
+y="holmes"
+xy=x + " " + y
+print(xy)
+info="Used for spacing===>1st method"
+
+X="144 \n" 
+Y="22344"
+print(X+Y)
+info="As spacing is considered as character we generally used it .===>2nd method"
+info2="\n is like 'br' tag in HTML"
+
+
+
+

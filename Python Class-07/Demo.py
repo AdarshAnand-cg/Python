e39fb893155d1861959f85aa02978cb26b4891d8
Adarsh <adarsh.anand.cg@gmail.com>
2026-09-01 17:07:19 +0530

    No Assignment in class 7.Already done in class 6 and a revision class occured.

---
+++ Python Class-07/Demo.py
@@ -0,0 +1,8 @@
+h="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla maiores odit cumque deserunt fugiat recusandae quae eaque in explicabo ipsa sed dolore iure voluptatibus consequuntur expedita, aliquid, natus placeat voluptates."
+print("Laudantium" in h)
+print("modi" in h)
+print("hero"not in h)
+print(h.find("modi"))
+print(h.count("ipsum"))
+print(h.startswith("Lorem"))
+print(h.endswith("fuga"))

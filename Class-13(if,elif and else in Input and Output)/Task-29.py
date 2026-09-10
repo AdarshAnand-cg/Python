87ddb6c4129998a91ca694b78a78b7795f2d9584
Adarsh <adarsh.anand.cg@gmail.com>
2026-09-10 11:54:18 +0530

    Assignment completed

--- Class-13(if,elif and else in Input and Output)/Task-29.py
+++ Class-13(if,elif and else in Input and Output)/Task-29.py
@@ -1,7 +1,13 @@
 # Task-29
+is_student=input("Are you student? (True/False)").lower().strip()
+is_student=input("Have ? (True/False)").lower().strip()
+has_tickets_ticket=input("Have you a ticket? (True/False)").lower().strip()
+
 is_student = True
 has_id = True
 has_ticket = True
 
-if is_student and has_id and has_ticket:
+if is_student :
     print("Allowed")
+
+

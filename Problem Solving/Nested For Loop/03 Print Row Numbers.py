442f8da4c68b9d93531498fc3e51ceed6e2bf070
Adarsh <adarsh.anand.cg@gmail.com>
2026-09-24 18:59:16 +0530

    Assignment Completed

---
+++ Problem Solving/Nested For Loop/03 Print Row Numbers.py
@@ -0,0 +1,4 @@
+for i in range(3):
+    for i in range(3):
+        print(j+1,end=" ")
+    print()
---
+++ Problem Solving/Nested For Loop/18 Print 1 to 20 in 4 Rows.py
@@ -0,0 +1,6 @@
+count=0
+for i in range(1,5):
+    for j in range(1,6):
+        count+=1
+        print(count,end=" ")
+    print()
